b03797932b0b4a04875643962e02fa3fbd1f74ca
vplakshmi <varalakshmi.pokala@gmail.com>
2019-10-31 07:52:12 -0400

    Add files via upload
    Added

---
+++ learningpython/13_functions/Factorial Function.py
@@ -0,0 +1,13 @@
+"""NSF - CODE"""
+
+#Factorial of a number
+
+def factorial(n):
+ # return 1 if the number is 0 or 1
+ # else return n!=n*(n-1)*(n-2)....*1
+
+ return 1 if (n == 1 or n == 0)\
+ else n * factorial(n-1);
+
+num = int(input("enter a number "));
+print("Factorial of number ", num, "is",factorial(num));
